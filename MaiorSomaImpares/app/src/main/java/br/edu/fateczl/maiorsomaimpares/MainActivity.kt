8abadf5e995ddd26c2a8e46052a4cb90ab438eb9
gdepaulay@hotmail.com <gdepaulay@hotmail.com>
2024-06-11 02:03:34 -0300

    Maior Valor e Somar os Impares entre os dois numeros

---
+++ MaiorSomaImpares/app/src/main/java/br/edu/fateczl/maiorsomaimpares/MainActivity.kt
@@ -0,0 +1,47 @@
+package br.edu.fateczl.maiorsomaimpares
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.edu.fateczl.maiorsomaimpares.ui.theme.MaiorSomaImparesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MaiorSomaImparesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                            name = "Android",
+                            modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MaiorSomaImparesTheme {
+        Greeting("Android")
+    }
+}
