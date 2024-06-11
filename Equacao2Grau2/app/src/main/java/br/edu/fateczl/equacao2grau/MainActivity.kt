c1f2f6ddd88868aa80237ed9cef90195a0e30490
gdepaulay@hotmail.com <gdepaulay@hotmail.com>
2024-06-11 02:56:01 -0300

    Equacao de segundo grau

---
+++ Equacao2Grau2/app/src/main/java/br/edu/fateczl/equacao2grau/MainActivity.kt
@@ -0,0 +1,47 @@
+package br.edu.fateczl.equacao2grau
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.edu.fateczl.equacao2grau.ui.theme.Equacao2GrauTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Equacao2GrauTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Equacao2GrauTheme {
+        Greeting("Android")
+    }
+}
